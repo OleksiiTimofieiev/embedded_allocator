3f341284b3da711b008aa18f74e7f048efcbaf3f
contrbas42 <contrbas@gmail.com>
2018-11-01 16:01:55 +0200

    to handle 5, 6, 7, 8, 9, 10

--- srcs/write2.c
+++ srcs/write2.c
@@ -37,7 +37,8 @@ bool	memory_availability(t_memory *memory)
 		return (false);
 	}
 
-	int possible_blocks = MEMORY_SIZE / BLOCK_SIZE; // check this stuff; // different politics; // mod // len != remaining size of the block 
+	int possible_blocks = MEMORY_SIZE / BLOCK_SIZE; 
+	// check this stuff; // different politics; // mod // len != remaining size of the block 
 	// len of str, remaining len of the block // poiter size remainging
 
 	possible_blocks *= BLOCK_SIZE;
